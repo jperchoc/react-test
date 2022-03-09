ee851316bc08f695e629d9c1f47aeb406167f232
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-03-09 20:45:22 +0100

    first draft

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import "./App.scss";
+import contact from "./img/contact.jpg"
+function App() {
+  return (
+    <div className="App container">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" href="#">
+          LOGO
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav mr-auto">
+            <a className="nav-item nav-link active" href="#home">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="#services">
+              Services
+            </a>
+            <a className="nav-item nav-link" href="#apropos">
+              Nous
+            </a>
+            <a className="nav-item nav-link" href="#gallerie">
+              Gallerie
+            </a>
+            <a className="nav-item nav-link" href="#contact">
+              Contact
+            </a>
+          </div>
+          <span className="navbar-text">
+            <a className="phone d-flex align-items-center" href="tel:+33123456789">
+              <span className="material-icons mr-2">call</span>01.23.45.67.89
+            </a>
+            <a className="mail d-flex align-items-center mt-1" href="mailto:toto@pwet.com">
+              <span className="material-icons mr-2">mail</span>toto@pwet.com
+            </a>
+          </span>
+        </div>
+      </nav>
+      <div className="row" id="home">
+        <div className="section-separator">
+          <span>Bienvenue</span>
+        </div>
+        <div className="col">
+          <img
+            src="https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/8/5/8/858743bb35_50169458_chien-min.jpg"
+            className="img-fluid"
+          />
+        </div>
+      </div>
+      <div className="row mt-3" id="services">
+        <div className="section-separator">
+          <span>Services</span>
+        </div>
+        <div className="col">
+          <div className="services d-flex flex-wrap">
+            <div className="service col">
+              <div className="info">
+                <div className="service-title-1">FULL SERVICE</div>
+                <div className="service-title-2">GROOM</div>
+                <div className="service-description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis praesentium ratione dicta ipsam similique assumenda commodi omnis optio maiores repellendus?
+                </div>
+                <a href="#">More</a>
+              </div>
+              <img
+                src="https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/8/5/8/858743bb35_50169458_chien-min.jpg"
+                className="img-fluid"
+              />
+            </div>
+            <div className="service col">
+              <div className="info">
+                <div className="service-title-1">FULL SERVICE</div>
+                <div className="service-title-2">BATH</div>
+                <div className="service-description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, molestiae.
+                </div>
+                <a href="#">More</a>
+              </div>
+              <img
+                src="https://static.actu.fr/uploads/2019/10/chien.jpg"
+                className="img-fluid"
+              />
+            </div>
+            <div className="service col">
+              <div className="info">
+                <div className="service-title-1">GoGo</div>
+                <div className="service-title-2">Express</div>
+                <div className="service-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa amet ex delectus perferendis quis!
+                </div>
+                <a href="#">More</a>
+              </div>
+              <img
+                src="https://www.chico.ca/wp-content/uploads/2021/04/landing-chien.png"
+                className="img-fluid"
+              />
+            </div>
+            <div className="service col">
+                <div className="info">
+                <div className="service-title-1">SERVICE</div>
+                <div className="service-title-2">A la carte</div>
+                <div className="service-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores ducimus quia et delectus!
+                </div>
+                <a href="#">More</a>
+              </div>
+              <img
+                src="https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__480.jpg"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3" id="apropos">
+        <div className="section-separator">
+          <span>Mewa</span>
+        </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis inventore perspiciatis sunt, unde ipsum possimus odit quos magnam eaque doloribus ea cupiditate excepturi quas facilis deserunt rem reiciendis porro quia. Cumque optio beatae unde ullam adipisci quam dolore itaque? Iure esse expedita debitis in repellat beatae voluptatum vitae! Impedit quis molestias facilis, dicta voluptates ad laboriosam facere eligendi culpa magni! Consectetur repellendus nulla rem quod aut aperiam officiis voluptatibus doloribus veritatis enim? Dolorum eaque et laborum ducimus, dolore similique veritatis voluptatum sunt aperiam obcaecati deserunt distinctio consectetur iusto sequi dolorem laudantium, earum quos voluptas quia? Dolores non ad et modi voluptas debitis illo similique fuga? Dolorem rerum quis amet! Corporis assumenda maxime itaque tempora, adipisci dignissimos. Molestiae vel esse, eum voluptates, voluptate quo iusto magni totam autem consequatur eligendi, nostrum quam. Ex maiores nesciunt, dolor magnam reiciendis, hic, culpa dicta expedita aut voluptatum blanditiis? Quaerat reiciendis facere nemo vel? Qui laboriosam autem veritatis et possimus nobis libero aliquam mollitia, cum, quis, esse ad quaerat! Earum maiores incidunt porro optio provident doloribus quia quo cum nemo quam harum illo officia sit laudantium, quaerat vel et odit eaque voluptatem esse necessitatibus in ex ipsum ab. In accusamus suscipit consectetur explicabo provident dolor illo, blanditiis minima. Culpa at fuga placeat, dolorum, incidunt illum harum, maxime minima sunt praesentium rerum quod velit excepturi illo tempora officia vero alias! Est sint quam dolores et ipsum error deleniti vero iure sit quo placeat impedit nobis sed porro dolore beatae laborum ut qui dolorum, provident ab, nisi ex? Provident repudiandae numquam ab sapiente, molestiae illum id sed mollitia sit, unde explicabo adipisci repellat voluptate enim nemo dolor vel ducimus dolorem neque? Minima, veritatis voluptatum similique omnis magni, alias officiis voluptatibus, nihil totam perspiciatis quaerat dolorem sit repellendus inventore est! Tempore similique provident a accusantium neque obcaecati laudantium!</p>
+        
+      </div>
+      <div className="row mt-3" id="gallerie">
+        <div className="section-separator">
+          <span>Photos de Ienchs</span>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi natus minima minus nesciunt sed molestias soluta quam impedit dolorum? Numquam doloremque beatae similique saepe illum repudiandae a molestiae consectetur provident optio, eligendi nesciunt quae animi modi tempora, perferendis cumque voluptatibus! Hic blanditiis adipisci aut architecto a dolores quasi, inventore quisquam? Impedit deserunt porro perspiciatis hic! Quo, exercitationem. A reprehenderit adipisci tempora eum corrupti voluptatibus laudantium earum officiis placeat, eligendi dignissimos omnis sed nulla in impedit autem sequi architecto, molestias odio numquam necessitatibus voluptatum hic delectus veritatis? Eum doloremque sint quas ipsum accusamus repellendus? Incidunt commodi eaque neque molestias eos recusandae accusantium debitis aperiam pariatur, officiis corporis voluptatum, itaque quis officia voluptas maxime, eveniet alias consectetur enim autem. Saepe delectus placeat vel iure corporis a cum odio. Similique enim libero voluptatem non, facilis sed illum sequi maiores quos ipsum quo, exercitationem veritatis quibusdam et, aliquam distinctio fuga. Unde ratione maxime reiciendis aliquam omnis earum reprehenderit? Eos cumque tenetur molestiae culpa numquam commodi alias accusamus voluptates modi nulla ea ipsum repudiandae ad, veniam provident dolore natus? Saepe repellendus corporis assumenda laboriosam quam, nobis minus dicta ipsa voluptatibus quae temporibus nulla vero at eius ab tempore rem ipsam est id omnis perferendis consequuntur?</p>
+      </div>
+      <div className="row mt-3 mb-3" id="contact">
+        <div className="section-separator">
+          <span>On se bigophone ?</span>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img src={contact} className="img-fluid" />
+          </div>
+          <div className="col">
+              <a className="phone d-flex align-items-center" href="tel:+33123456789">
+                    <span className="material-icons mr-2">call</span>01.23.45.67.89
+                  </a>
+                  <a className="mail d-flex align-items-center mt-1" href="mailto:toto@pwet.com">
+                    <span className="material-icons mr-2">mail</span>toto@pwet.com
+                  </a>
+            </div>
+        </div>
+       
+        </div>
+    </div>
+  );
+}
+
+export default App;
